73ee9d4287dd198712a29aa1df74f430f7cc7a87
sturchanskiy <turchanskiy777@gmail.com>
2020-11-19 12:09:06 +0200

    added security user; added when user can upload images;

---
+++ src/main/java/com/example/sweater/repos/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.sweater.repos;
+
+import com.example.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    static User findByActivationCode(String code) {
+        return null;
+    }
+
+}
